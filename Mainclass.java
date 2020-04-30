90bb0e61d0bb4a6153c1eabe4b1311998557abe3
Saket29 <saket.dhamapurkar29@gmail.com>
2020-04-30 21:16:19 +0530

    Commit

---
+++ Mainclass.java
@@ -0,0 +1,13 @@
+package hangman;
+
+public class Mainclass
+{
+    public static void main(String[] args) 
+    {
+        Gamemenu gm=new Gamemenu();
+        gm.addmenucomponent();
+    }
+
+}
+
+
